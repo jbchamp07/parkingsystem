d222363c2f005fd8a02192892ccf51a42d27c2ea
Champetier <jb.champetier@gmail.com>
2021-12-15 18:54:36 +0100

    test passed

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,6 +3,10 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+
 public class FareCalculatorService {
 
     public void calculateFare(Ticket ticket){
@@ -14,7 +18,20 @@ public class FareCalculatorService {
         int outHour = ticket.getOutTime().getHours();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        LocalDateTime ldtIn = LocalDateTime.ofInstant(ticket.getInTime().toInstant(),
+                ZoneId.systemDefault());
+        LocalDateTime ldtOut = LocalDateTime.ofInstant(ticket.getOutTime().toInstant(),
+                ZoneId.systemDefault());
+        int differenceInMinutes = (int) ChronoUnit.MINUTES.between(ldtIn, ldtOut);
+        int differenceInDays = (int) ChronoUnit.DAYS.between(ldtIn, ldtOut);
+        int differenceInHeures = 0;
+        if(differenceInMinutes / 60 > 0){
+            differenceInHeures = differenceInMinutes / 60;
+        }
+        if(differenceInMinutes == 60){
+            differenceInMinutes = 0;
+        }
+        double duration = differenceInHeures + ((double)differenceInMinutes/60) - (differenceInHeures * differenceInDays);
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
